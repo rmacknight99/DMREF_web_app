04992f2f64824eec6f650b2b8b0b9b3905ba7474
rmacknight99 <rmacknight18@gmail.com>
2023-02-14 14:29:54 -0500

    made df

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ from sklearn.datasets import load_wine
 from sklearn.decomposition import PCA
 import plotly.graph_objects as go
 
+def load_descriptors(in_file):
+
+    descriptors = pd.read_csv(in_file)
+    return descriptors, descriptors.columns
 
 # Title
 st.title("PCA applied to Chemical Space of Amine Salts")
+df, cols = load_descriptors("descriptors_ORCA_13.csv")
